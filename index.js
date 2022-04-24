6b0458660051a8070e3238f951190d6af2480439
AdrianFred <adrian989@me.com>
2022-04-23 23:45:33 -0500

    version mexico

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  "https://stoicmedia.no/cross-course/wp-json/wc/v3/products?consumer_key=ck_ee54b6d16a6371de23d6c83d1c28d514d3bdc101&consumer_secret=cs_f8f386698e53ca286404328bc0b76aec6dd18246&per_page=4";
+  "https://stoicmedia.no/cross-course/wp-json/wc/v3/products?consumer_key=ck_ee54b6d16a6371de23d6c83d1c28d514d3bdc101&consumer_secret=cs_f8f386698e53ca286404328bc0b76aec6dd18246&per_page=20";
 var productContainer = document.querySelector("#mens");
 productContainer.innerHTML = "";
 
@@ -8,7 +8,7 @@ async function getProducts(url) {
   const products = await response.json();
   console.log(products);
   products.forEach(function (product) {
-    if (product.featured) {
+    if (product.featured && product.categories[0].slug === "mens") {
       productContainer.innerHTML += `
     <a href="productspecific.html?id=${product.id}"><img src="${product.images[0].src}" alt="Image of a jacket" class="jacket-image" /></a>
     `;
@@ -27,7 +27,7 @@ async function getProductss(url) {
   const products = await response.json();
   console.log(products);
   products.forEach(function (product) {
-    if (product.featured) {
+    if (product.featured && product.categories[0].slug === "womens") {
       productContainer1.innerHTML += `
     <a href="productspecific.html?id=${product.id}"><img src="${product.images[0].src}" alt="Image of a jacket" class="jacket-image" /></a>
     `;
--- women.js
+++ women.js
@@ -1,4 +1,5 @@
-const baseUrl = "https://stoicmedia.no/cross-course/wp-json/wc/store/products";
+const baseUrl =
+  "https://stoicmedia.no/cross-course/wp-json/wc/v3/products?consumer_key=ck_ee54b6d16a6371de23d6c83d1c28d514d3bdc101&consumer_secret=cs_f8f386698e53ca286404328bc0b76aec6dd18246&per_page=20&orderby=title&order=asc";
 const productContainer = document.querySelector(".jacket-list");
 productContainer.innerHTML = "";
 
@@ -7,12 +8,14 @@ async function getProducts(url) {
   const products = await response.json();
   console.log(products);
   products.forEach(function (product) {
-    productContainer.innerHTML += `
+    if (product.categories[0].slug === "womens") {
+      productContainer.innerHTML += `
     <div>
     <a href="productspecific.html?id=${product.id}"><img src="${product.images[0].src}" alt="Image of a jacket" class="jacket-image" /></a>
     <p>${product.name}</p>
     <p>${product.price_html}</p>
     </div>`;
+    }
   });
 }
 
